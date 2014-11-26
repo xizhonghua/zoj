b69f255b9a31715fbd4879fa4e4d77cdc4433601
Zhonghua Xi <xizhonghua@gmail.com>
2014-11-26 14:27:14 -0500

    ac 3793

---
+++ vol_28/3793.cpp
@@ -0,0 +1,13 @@
+// 3829423  2014-11-27 03:20:28 Accepted    3793    C++0x   70  272 xxfflower
+#include <iostream>
+using namespace std;
+
+int main(){
+    int t,a,b;
+    cin>>t;
+    while(t--) {
+        cin>>a>>b;
+        cout<<1<<endl;
+    }
+    return 0;
+}
