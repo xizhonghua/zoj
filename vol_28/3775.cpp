f4d390597f48117c08b0bcf8fed5893e19f9bda7
Zhonghua Xi <xizhonghua@gmail.com>
2015-01-07 15:26:14 -0500

    ac 3775

---
+++ vol_28/3775.cpp
@@ -0,0 +1,35 @@
+//3847612   2015-01-08 04:18:16 Accepted    3775    C++0x   0   272 xxfflower
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <map>
+using namespace std;
+
+class Program
+{
+public:    
+    static string run(const string& source) {
+        string output;
+        for(int i=0;i<source.length();i++)
+            switch(source[i]) {
+                case '_': output += source; break;
+                case '!': output += "Hello, world!"; break;
+            }
+        return output;
+    }
+private:
+    string _source;
+};
+
+int main(int argc, char** argv) {
+    int t;
+    string source;
+    cin>>t;    
+    getline(cin, source);
+    while(t--) {        
+        getline(cin, source);
+        cout<<((Program::run(source)==source)?"Yes":"No")<<endl;            
+    }
+    return 0;
+}
